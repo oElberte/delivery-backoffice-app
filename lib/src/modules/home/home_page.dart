87aaa60178a526cd6bdf3a3ed0df4385c7128f13
Elberte Plinio <elbertedoolan@gmail.com>
2023-06-03 09:39:23 -0300

    added .env and helpers

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import '../../core/env/env.dart';
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
+import '../../core/ui/helpers/size_extensions.dart';
 
-  const HomePage({ super.key });
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Home Page'),),
-           body: Container(),
-       );
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(
+            const Duration(seconds: 2),
+          );
+          hideLoader();
+          showError('Erro de alguma coisa');
+        },
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.red,
+              width: context.percentWidth(.5),
+              height: context.percentHeight(.9),
+            ),
+            Text(
+              Env.instance.get('backend_base_url'),
+            ),
+            Text(
+              context.screenWidth.toString(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
