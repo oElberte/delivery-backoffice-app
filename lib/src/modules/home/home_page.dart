63d42b893080dd0f8912401d755c51261f1ad699
Elberte Plinio <elbertedoolan@gmail.com>
2023-06-06 12:16:15 -0300

    added base route with RouterOutlet

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
-import '../template/base_layout.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,23 +13,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
-    return BaseLayout(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextFormField(
-              decoration: const InputDecoration(
-                label: Text('Login'),
-              ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              label: Text('Login'),
             ),
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Botão'),
-          ),
-        ],
-      ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Botão'),
+        ),
+      ],
     );
   }
 }
