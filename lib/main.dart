fa7acdf558e57b97e68730561cece36e3871e16f
Elberte Plinio <elbertedoolan@gmail.com>
2023-06-16 19:40:05 -0300

    implemented product data layer

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'src/app_module.dart';
 import 'src/app_widget.dart';
 import 'src/core/env/env.dart';
 
+//TODO: Dia 4 PARTE 1 - 27min
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Env.instance.load();
