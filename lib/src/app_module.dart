db8421d22aca050f3ea7e19a0be26484de3b1f84
Elberte Plinio <elbertedoolan@gmail.com>
2023-06-15 21:35:29 -0300

    added product home

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'modules/core/core_module.dart';
 import 'modules/login/login_module.dart';
 import 'modules/payment_type/payment_type_module.dart';
+import 'modules/products/products_module.dart';
 import 'modules/template/base_layout.dart';
 import 'modules/template/menu/menu_enum.dart';
 
@@ -22,10 +23,15 @@ class AppModule extends Module {
           child: (context, args) => const BaseLayout(
             body: RouterOutlet(),
           ),
+          transition: TransitionType.noTransition,
           children: [
             ModuleRoute(
               Menu.paymentType.route,
               module: PaymentTypeModule(),
+            ),
+            ModuleRoute(
+              Menu.products.route,
+              module: ProductsModule(),
             ),
           ],
         ),
